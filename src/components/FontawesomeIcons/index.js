e14a0b31b2778e54885297798d7c3ef2053b11e1
tmorriso93 <tmorriso93@gmail.com>
2021-01-17 03:22:40 -0500

    added fontawesome

---
+++ src/components/FontawesomeIcons/index.js
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faMapMarker, faPlane, faCheckCircle, faGlobe, faClock, faMoneyCheck } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faMapMarker, faPlane, faCheckCircle, faGlobe, faClock, faMoneyCheck);
